6c77132c5a6cafd041a3bd7160e9bdd6f4875049
kcorbettguilford <kcorbett@guilford.edu>
2025-02-24 21:12:51 -0500

    step 2 done but second button won't work

--- testscript.js
+++ testscript.js
@@ -1,7 +1,27 @@
 myitem = document.getElementById("firsttest");
+
 myitem.addEventListener("click", onClick);
+
 function onClick() {  
     myitem.style.color = "green";
     myitem.style.backgroundColor = "whitesmoke"
 // Event detail goes green and whitesmoke background
 }
+thebutton = document.getElementById("thebutton");  
+otheritem = document.getElementById("buttontest");
+
+thebutton.addEventListener("click", onButtonClick);
+
+function onButtonClick() {  
+     otheritem.style.color = "red"; 
+}
+thebutton = document.getElementById("thebutton");  
+otheritem = document.getElementById("buttontest");
+
+thebutton.addEventListener("click", onButtonClick);
+
+function onButtonClick() {  
+     otheritem.style.color = "red"; 
+}
+
+
