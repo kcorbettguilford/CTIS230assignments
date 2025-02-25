3fe7da30fc0cd24d0522ae7330e4f3f0ff9b6c27
kcorbettguilford <kcorbett@guilford.edu>
2025-02-24 20:18:35 -0500

    part 1 done

---
+++ testscript.js
@@ -0,0 +1,7 @@
+myitem = document.getElementById("firsttest");
+myitem.addEventListener("click", onClick);
+function onClick() {  
+    myitem.style.color = "green";
+    myitem.style.backgroundColor = "whitesmoke"
+// Event detail goes green and whitesmoke background
+}
